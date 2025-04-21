9b3ab05709f0f87371d15a580f5b48037d84c4d6
ijbraun <ijbraun91@gmail.com>
2025-04-21 17:13:26 -0400

    added button behavior

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -3,7 +3,7 @@ import { useSelector, useDispatch } from 'react-redux';
 import { removeItem, updateQuantity } from './CartSlice';
 import './CartItem.css';
 
-const CartItem = ({ onContinueShopping }) => {
+const CartItem = ({ addedToCart, onContinueShopping }) => {
     const cart = useSelector(state => state.cart.items);
     const dispatch = useDispatch();
 
@@ -36,11 +36,13 @@ const CartItem = ({ onContinueShopping }) => {
         }
         else{
             dispatch(removeItem(item.name));
+            addedToCart[item.name]=false;
         }
     };
 
     const handleRemove = (item) => {
         dispatch(removeItem(item.name));
+        addedToCart[item.name]=false;
     };
 
     // Calculate total cost based on quantity for an item
@@ -52,7 +54,8 @@ const CartItem = ({ onContinueShopping }) => {
 
     return (
         <div className="cart-container">
-        <h2 style={{ color: 'black' }}>Total Cart Amount: ${calculateTotalAmount()}</h2>
+            <h2 style={{ color: 'black' }}>Total Cart Amount: ${calculateTotalAmount()}</h2>
+            <h2 style={{ color: 'black' }}>Total Number of Plants: {cart.reduce((acc, item)=>{return acc+=item.quantity},0)}</h2>
             <div>
             {cart.map(item => (
                 <div className="cart-item" key={item.name}>
@@ -61,9 +64,9 @@ const CartItem = ({ onContinueShopping }) => {
                     <div className="cart-item-name">{item.name}</div>
                     <div className="cart-item-cost">{item.cost}</div>
                     <div className="cart-item-quantity">
-                    <button className="cart-item-button cart-item-button-dec" onClick={() => handleDecrement(item)}>-</button>
-                    <span className="cart-item-quantity-value">{item.quantity}</span>
-                    <button className="cart-item-button cart-item-button-inc" onClick={() => handleIncrement(item)}>+</button>
+                        <button className="cart-item-button cart-item-button-dec" onClick={() => handleDecrement(item)}>-</button>
+                        <span className="cart-item-quantity-value">{item.quantity}</span>
+                        <button className="cart-item-button cart-item-button-inc" onClick={() => handleIncrement(item)}>+</button>
                     </div>
                     <div className="cart-item-total">Total: ${calculateTotalCost(item)}</div>
                     <button className="cart-item-delete" onClick={() => handleRemove(item)}>Delete</button>
@@ -73,9 +76,9 @@ const CartItem = ({ onContinueShopping }) => {
             </div>
             <div style={{ marginTop: '20px', color: 'black' }} className='total_cart_amount'></div>
             <div className="continue_shopping_btn">
-            <button className="get-started-button" onClick={(e) => handleContinueShopping(e)}>Continue Shopping</button>
-            <br />
-            <button className="get-started-button1" onClick={(e) => handleCheckoutShopping(e)}>Checkout</button>
+                <button className="get-started-button" onClick={(e) => handleContinueShopping(e)}>Continue Shopping</button>
+                <br />
+                <button className="get-started-button1" onClick={(e) => handleCheckoutShopping(e)}>Checkout</button>
             </div>
         </div>
   );
--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -2,12 +2,12 @@ import React, { useState, useEffect } from 'react';
 import './ProductList.css'
 import CartItem from './CartItem';
 import { addItem } from './CartSlice';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 function ProductList({ onHomeClick }) {
     const [showCart, setShowCart] = useState(false);
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
-    const [addToCart, setAddedToCart] = useState({});
+    const [addedToCart, setAddedToCart] = useState({});
     const dispatch = useDispatch();
     const plantsArray = [
         {
@@ -216,6 +216,7 @@ function ProductList({ onHomeClick }) {
             ]
         }
     ];
+    const cart = useSelector(state => state.cart.items)
     const styleObj = {
         backgroundColor: '#4CAF50',
         color: '#fff!important',
@@ -281,7 +282,19 @@ function ProductList({ onHomeClick }) {
                 </div>
                 <div style={styleObjUl}>
                     <div> <a href="#" onClick={(e) => handlePlantsClick(e)} style={styleA}>Plants</a></div>
-                    <div> <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}><h1 className='cart'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path></svg></h1></a></div>
+                    <div> 
+                        <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}>
+                            <h1 className='cart'>
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68">
+                                    <rect width="156" height="156" fill="none"></rect>
+                                    <text x="110" y="150" fill='white'>{cart.reduce((acc, item)=>{return acc+=item.quantity},0)}</text>
+                                    <circle cx="80" cy="216" r="12"></circle>
+                                    <circle cx="184" cy="216" r="12"></circle>
+                                    <path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path>
+                                </svg>
+                            </h1>
+                        </a>
+                    </div>
                 </div>
             </div>
             {!showCart ? (
@@ -296,7 +309,8 @@ function ProductList({ onHomeClick }) {
                                         <div className='product-title'>{plant.name}</div>
                                         <div className='product-description'>{plant.description}</div>
                                         <div className='product-cost'>{plant.cost}</div>
-                                        <button className='product-button' onClick={() => handleAddToCart(plant)}> Add to Cart</button>
+                                        
+                                        <button disabled={addedToCart[plant.name]} className={(addedToCart[plant.name])?'product-button added-to-cart':'product-button'} onClick={() => handleAddToCart(plant)}>{(addedToCart[plant.name])?"Added to Cart":"Add to Cart"}</button>
                                     </div>
                                 ))}
                             </div>
@@ -304,7 +318,7 @@ function ProductList({ onHomeClick }) {
                     ))}
                 </div>
             ) : (
-                <CartItem onContinueShopping={handleContinueShopping} />
+                <CartItem addedToCart={addedToCart} onContinueShopping={handleContinueShopping} />
             )}
         </div>
     );
